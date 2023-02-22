4250e31d1fc8c813d474c3449c3797fb7f5d052b
cjsolis <cjelizondo12@gmail.com>
2023-02-22 09:39:58 -0600

    added user prompt for for many characters they want

--- Develop/script.js
+++ Develop/script.js
@@ -2,7 +2,7 @@
 
 // Array of options for password
 
-let specialChar = ["!", "#", "$", "%", "'", "&", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"];
+let uniqueChars = ["!", "#", "$", "%", "'", "&", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"];
 let upperCase = ["A", "B", "C", "D", "E","F","G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V","W", "X", "Y", "Z"];
 let lowerCase = ["a", "b", "c", "d", "e","f","g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v","w", "x", "y", "z"];
 let num = [1,2,3,4,5,6,7,8,9,0];
@@ -10,18 +10,25 @@ let num = [1,2,3,4,5,6,7,8,9,0];
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-function generatePassword(){
-  // let length = prompt(`How many characters will your password be? Enter a number from 8-128`)
-  //   if(length < 8 || length >)
-  // let specialChar = prompt()
 
+// User prompts for criteria
+let length = prompt(`How many characters will your password be? Enter a number from 8-128`)
+if(length < 8 || length >128){
+  alert("Sorry, password must be between 8 and 128 character long.")
 }
+let specialChar = prompt(`How many special characters do you want in your password? Must have at least 1`)
+
+
+function generatePassword(){
+  
+ 
+}
+
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 
 }
