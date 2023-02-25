07670c82cc421353182d9a57bf210827b2d109f9
cjsolis <cjelizondo12@gmail.com>
2023-02-25 10:42:29 -0600

    added function in while loop if user clicks cancel on everything

--- Develop/script.js
+++ Develop/script.js
@@ -7,52 +7,68 @@ var lower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q"
 var num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 var randomPassword = [];
 var password = ""
-
+var selection = false;
+let characterLength = 0;
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-function generatePassword() {
-  // User prompts for criteria
-
-  // Length of the password user wants
-    let characterLength = parseInt(prompt(`How many characters will your password be? Enter a number from 8-128`));
+var gatherOptions = function(){
+  characterLength = parseInt(prompt(`How many characters will your password be? Enter a number from 8-128`));
       while( isNaN(characterLength) ||characterLength < 8 || characterLength > 128) {
         alert( `Character length should be a number and between 8 - 128 digits. Try again.`);
         characterLength = parseInt(prompt(`How many characters will your password be? Enter a number from 8-128`));
     } 
+  // Does user want special characters
+    let useSpecialChar = confirm(`How many special characters do you want in your password? Must pick at least 1`);
+      
+      if(useSpecialChar){
+        randomPassword = randomPassword.concat(uniqueChars)
+        selection = true;
+      }
 
     // does user want Uppercase letters
     let useUpperCase = confirm(`Do you want Uppercase letters in your password?`)
       if(useUpperCase){
       randomPassword = randomPassword.concat(upper);
+      selection = true;
       }
     // Does user want lowercase letters
     let useLowerCase = confirm(`Do you want Lowercase Letters in your password?`)
         if(useLowerCase){
         randomPassword = randomPassword.concat(lower);
+        selection = true;
       }
     
       // Does user want numbers
       let useNum = confirm(`Do you want numbers in your password?`)
-      if(useNum){
-      randomPassword = randomPassword.concat(num);
+        if(useNum){
+        randomPassword = randomPassword.concat(num);
+        selection = true;
+      }
+}
+
+function generatePassword() {
+  // User prompts for criteria
+  while(selection === false){
+    gatherOptions();
+    if(selection === false){
+     alert(`Need to select criteria`);
     }
-    
-    
-      console.log(randomPassword)
-    
-    // for(let i = 0; i < length; i++){
-    //   let index = Math.floor(Math.random() * randomPassword.length)
-    //   password = password + randomPassword[index]
-    // }
-    // return password
+  }
+    for(let i = 0; i < characterLength; i++){
+      let index  = Math.floor(Math.random() * randomPassword.length -1)
+      password = password.concat(randomPassword[index])
+      console.log(password)
+    }
+    return password
 }
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  selection = false;
+  var finalPassword = generatePassword();
   var passwordText = document.querySelector("#password");
-  passwordText.value = password;
+  passwordText.value = finalPassword;
 }
 
 // Add event listener to generate button
