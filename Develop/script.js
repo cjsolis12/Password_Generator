a1b137b6c99c388ef5ba58cd60cbaddb09c203c7
cjsolis <cjelizondo12@gmail.com>
2023-02-26 11:39:09 -0600

    fixed indentations in js file

--- Develop/script.js
+++ Develop/script.js
@@ -9,52 +9,54 @@ var randomPassword = [];
 var password = ""
 var selection = false;
 let characterLength = 0;
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+// Gather all the user inputs
 var gatherOptions = function(){
+ 
   characterLength = parseInt(prompt(`How many characters will your password be? Enter a number from 8-128`));
       while( isNaN(characterLength) ||characterLength < 8 || characterLength > 128) {
         alert( `Character length should be a number and between 8 - 128 digits. Try again.`);
         characterLength = parseInt(prompt(`How many characters will your password be? Enter a number from 8-128`));
     } 
-  // Does user want special characters
-    let useSpecialChar = confirm(`How many special characters do you want in your password? Must pick at least 1`);
-      
-      if(useSpecialChar){
+// Does user want special characters
+  let useSpecialChar = confirm(`How many special characters do you want in your password? Must pick at least 1`);
+    if(useSpecialChar){
         randomPassword = randomPassword.concat(uniqueChars)
         selection = true;
       }
 
-    // does user want Uppercase letters
-    let useUpperCase = confirm(`Do you want Uppercase letters in your password?`)
-      if(useUpperCase){
-      randomPassword = randomPassword.concat(upper);
-      selection = true;
+// does user want Uppercase letters
+  let useUpperCase = confirm(`Do you want Uppercase letters in your password?`)
+    if(useUpperCase){
+        randomPassword = randomPassword.concat(upper);
+        selection = true;
       }
-    // Does user want lowercase letters
-    let useLowerCase = confirm(`Do you want Lowercase Letters in your password?`)
-        if(useLowerCase){
+// Does user want lowercase letters
+  let useLowerCase = confirm(`Do you want Lowercase Letters in your password?`)
+    if(useLowerCase){
         randomPassword = randomPassword.concat(lower);
         selection = true;
       }
-    
-      // Does user want numbers
-      let useNum = confirm(`Do you want numbers in your password?`)
-        if(useNum){
+// Does user want numbers
+  let useNum = confirm(`Do you want numbers in your password?`)
+    if(useNum){
         randomPassword = randomPassword.concat(num);
         selection = true;
       }
 }
 
 function generatePassword() {
-  // User prompts for criteria
-  while(selection === false){
-    gatherOptions();
-    if(selection === false){
-     alert(`Need to select criteria`);
+ // User prompts for criteria
+    while(selection === false){
+        gatherOptions();
+        if(selection === false){
+          alert(`Need to select criteria`);
     }
   }
+// Generate a random password with user criteria
     for(let i = 0; i < characterLength; i++){
       let index  = Math.floor(Math.random() * randomPassword.length -1)
       password = password.concat(randomPassword[index])
@@ -74,9 +76,3 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-// 1.  Prompt the user for the password criteria (prompts)
-//   - length 8 - 128 characters (conditional)
-//   - lowercase, uppercase, numbers, special characters (arrays)
-// 2. Validate the input (check that inputs meet criteria)
-// 3. Generate the password (randomized)
-// 4. Display the generated password on the page
